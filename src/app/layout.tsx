ed128dd4c7411a43a379cc292872b7dc70e976ed
Zach Wise <zach@digitalartwork.net>
2026-03-04 23:06:09 -0600

    refactor: Move Google Fonts import to `layout.tsx` and update CSS font declarations to directly reference font names.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -42,6 +42,11 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en" className="dark">
+            <head>
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+                <link href="https://fonts.googleapis.com/css2?family=Oxanium:wght@200..800&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet" />
+            </head>
             <body
                 className="antialiased bg-background text-foreground"
             >
